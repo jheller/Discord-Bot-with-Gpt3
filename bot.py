bccaf095c562db14515b0253e4e87d16b6a29a04
John Heller <john@heller.com.au>
2022-01-09 15:21:22 +1100

    Remove token

--- bot.py
+++ bot.py
@@ -19,16 +19,10 @@ list_user = []
 async def on_message(message):
     if message.channel.id == message.author.dm_channel.id: # dm only
         #await message.channel.send('ping')
-        # chat_log = ""
         list_user.append(message.author.id)
         question = message.content
         answer = OpenAi.ask(question)
-        # chat_log = OpenAi.append_interaction_to_chat_log(question, answer,chat_log)
         await message.channel.send(answer)
-        # f = open("log_user.txt", "w")
-        # for it in list_user:
-        # 	f.write("%i\n" % it)
-        # f.close()
 
 
 @client.command()
@@ -37,4 +31,4 @@ async def shutdown(context):
     exit()
 
 
-client.run("OTI4OTE0MTAwOTYzMTM5NjI0.YdfsrA.QZhSRV7aSglhXiPjQQsjuOiIl7k")
+client.run("discord token")
